a1a88d5114ccffa14271f38ff29aac5aa0f6cd2a
Camila Belen <camilabelencapua@gmail.com>
2022-07-07 19:18:30 -0300

    lowerToUpper and upperToLower

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -17,10 +17,18 @@
 
           <br>
             <h5> Cantidad:  {{countLetters}}</h5>
+
+          <br>
+            <h5>{{changeVowel}} (Codificado)</h5>
+          
           <br>
             <h5>{{upperCase}} (Mayúscula)</h5>
+          
           <br>
-            <h5>{{lowerCase}} (Minúscula)</h5>
+            <h5>{{upperToLower}}(Mayúscula/Minúscula)</h5>
+          
+          <br>
+          <h5>{{lowerToUpper}} (Minúscula/Mayúscula)</h5>
         <br>
       </div>
   </section>
@@ -54,13 +62,51 @@ export default {
       return this.formData.input.length;
     },
 
+    changeVowel(){
+      return  this.formData.input
+                    .toLowerCase()
+                    .replace(/a/g,'u') //g is global
+                    .replace(/e/g,"o")
+                    .replace(/o/g, "e")
+                    .replace(/u/g, "a")
+    },
+
     upperCase(){
       return this.formData.input.toUpperCase() ;
     },
 
     lowerCase(){
       return this.formData.input.toLowerCase() ;
+    },
+
+    upperToLower (){
+      const input = this.formData.input
+      let newWord = ''
+      for (let i=0; i<=input.length-1; i++) {
+        const letter = input[i]
+        if(i%2===0){
+          newWord += letter.toUpperCase();
+        }else{
+          newWord += letter.toLowerCase();
+        }
+      } 
+      return newWord
+    },
+
+    lowerToUpper(){
+      const input = this.formData.input
+      let newWord = ''
+      for (let i=0; i<=input.length-1; i++) {
+        const letter = input[i]
+        if(i%2===0){
+          newWord += letter.toLowerCase();
+        }else{
+          newWord += letter.toUpperCase();
+        }
+      } 
+      return newWord
     }
+
   },
   
 }
